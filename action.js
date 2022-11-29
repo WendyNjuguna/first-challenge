bf4c758f8401f7ed4470c3da97985c2667f248e6
Wendy <wendynjuguna714@gmail.com>
2022-11-29 09:19:03 +0300

    Completed Assignment

--- action.js
+++ action.js
@@ -1,1 +1,16 @@
+const prompt = require('prompt-sync')();
+const speed = prompt("Please Enter Car Speed:");
+let points = (speed - 70)/5;
+function speedDetector() {
+    if (speed < 70 ){
+    console.log ("Ok");
+}
+else if (points > 12) {
+    console.log("License suspended");
+}
+else {
+    console.log('Points: ${points}');
+}
+}
 
+speedDetector
--- see.js
+++ see.js
@@ -1,1 +1,24 @@
+// Call promt-sync in order to get the actual prompting function.
+const prompt = require("prompt-sync")();
+// Define variable score.
+// Prompt users to input student marks, which is stored in the store variable.
+const score = prompt("Please Input Student Marks: ");
+// Define a function studentGradeGenerator.
+function studentGradeGenerator() {
 
+  if (score > 79 && score < 100) {
+    console.log("A");
+  } else if (score >= 60 && score <= 79) {
+    console.log("B");
+  } else if (score >= 49 && score <= 59) {
+    console.log("C");
+  } else if (score >= 40 && score <= 48) {
+    console.log("D");
+  } else if (score >= 0 && score <= 39) {
+    console.log("E");
+  } else {
+    console.log("Invalid Input!");
+  }
+}
+//Call the function studentGradeGenerator.
+studentGradeGenerator();
